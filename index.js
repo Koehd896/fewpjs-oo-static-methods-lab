ab3f995e9a06fa91dbe375df843f41bcbab7cd64
Raven <ravenkoehler@gmail.com>
2021-09-15 08:39:08 -0400

    commit tree 22a0178239b512ed3ee6dfb6de6ffacd43b0817c

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^-A-Za-z0-9 ']+/g, '');
+  }
+
+  static titleize(str) {
+    const unchanged = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const strArray = str.split(" ");
+    const newArray = strArray.map(word => {
+      if (unchanged.find(w => w === word)) {
+        return word;
+      } else {
+        return Formatter.capitalize(word);
+      }
+    })
+    newArray[0] = Formatter.capitalize(newArray[0]);
+    return newArray.join(" ");
+  }
 }
